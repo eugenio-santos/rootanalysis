ddf7bd59bea1663effe51040540a3526e90fa707
eugenio santos <1evsantos8@gmail.com>
2021-05-29 11:12:04 +0100

    granger test, and data analisys

--- src/nonagregated/granger.py
+++ src/nonagregated/granger.py
@@ -1,94 +1,154 @@
+import os
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 import statsmodels.tsa.stattools as st
 import seaborn as sn
 
 
-df = pd.read_csv(
-    'C:/Users/1evsa/Desktop/M/proj/rootanalysis/dados/Mock_dataset/MockDataset_train.csv')
+def main(argv):
 
-df['DerivedFlag'] = df['DerivedFlag'].fillna(0)  # retirar nan da coluna
-df['Date'] = df['Date'].str.replace('-', '')
-df = df.astype({'DerivedFlag': 'int32'})
+    df = pd.read_csv(
+        'C:/Users/1evsa/Desktop/M/proj/rootanalysis/dados/Mock_dataset/MockDataset_train.csv')
 
-groups = pd.unique(df['GroupKey'])
+    df['DerivedFlag'] = df['DerivedFlag'].fillna(0)  # retirar nan da coluna
+    df['Date'] = df['Date'].str.replace('-', '')
+    df = df.astype({'DerivedFlag': 'int32'})
+
+    # data frames to save the best results from granger causality tests
+    df_ssrf = pd.DataFrame()
+    df_ssrchi = pd.DataFrame()
+    df_lrtest = pd.DataFrame()
+    df_parmf = pd.DataFrame()
+
+    groups = pd.unique(df['GroupKey'])
+    #groups = groups[:1]
+    for group in groups:
+        print(group)
+        gr = df[df['GroupKey'] == group]
+        pks = pd.unique(gr['PrimaryKey'])
+        root = df[df['PrimaryKey'] == group][['Date', 'Value']]
+
+        _ssrf = {}
+        _ssrchi = {}
+        _lrtest = {}
+        _parmf = {}
+
+        num_lags = 15
+        pks = pks[1:]  # remove A1(root) form keys
+        for key in pks:
+            flag = df.loc[(df['PrimaryKey'] == key), 'DerivedFlag'].values[0]
+            rel = df[df['PrimaryKey'] == key][['Date', 'Value']]
+            VAR = pd.merge(root, rel, how='outer', on='Date')
+
+            gc = st.grangercausalitytests(
+                VAR[['Value_x', 'Value_y']], maxlag=num_lags, verbose=False)
+
+            ssrf = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
+            ssrchi = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
+            lrtest = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
+            parmf = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
+
+            for lag, item in gc.items():
+                if item[0]['ssr_ftest'][0] > ssrf['score']:
+                    ssrf['score'] = item[0]['ssr_ftest'][0]
+                    ssrf['lag'] = lag
+                if item[0]['ssr_chi2test'][0] > ssrchi['score']:
+                    ssrchi['score'] = item[0]['ssr_chi2test'][0]
+                    ssrchi['lag'] = lag
+                if item[0]['lrtest'][0] > lrtest['score']:
+                    lrtest['score'] = item[0]['lrtest'][0]
+                    lrtest['lag'] = lag
+                if item[0]['params_ftest'][0] > parmf['score']:
+                    parmf['score'] = item[0]['params_ftest'][0]
+                    parmf['lag'] = lag
+
+            _ssrf[key] = ssrf
+            _ssrchi[key] = ssrchi
+            _lrtest[key] = lrtest
+            _parmf[key] = parmf
+
+        df_ssrf = pd.concat(
+            [df_ssrf, pd.DataFrame.from_dict(_ssrf, orient='index')])
+        df_ssrchi = pd.concat(
+            [df_ssrchi, pd.DataFrame.from_dict(_ssrchi, orient='index')])
+        df_lrtest = pd.concat(
+            [df_lrtest, pd.DataFrame.from_dict(_lrtest, orient='index')])
+        df_parmf = pd.concat(
+            [df_parmf, pd.DataFrame.from_dict(_parmf, orient='index')])
+
+    frames = [df_ssrf, df_ssrchi, df_lrtest, df_parmf]
+
+    print(df_ssrchi)
+
+    thresholds = range(9, 18)
+    tests = []
+    tests += (conf_m_analysis(df_ssrf, thresholds, 'ssr_ftest'))
+    #tests += (conf_m_analysis(df_ssrf, thresholds, 'ssr_chi2test'))
+    #tests += (conf_m_analysis(df_ssrf, thresholds, 'lrtest'))
+    #tests += (conf_m_analysis(df_ssrf, thresholds, 'params_ftest'))
+
+    t = pd.DataFrame(tests)
+    print(t)
+    print(t.sort_values(by=['kappa'], ascending=False))
+    print(t.sort_values(by=['acc'], ascending=False))
 
 
-# data frames to save the best results from granger causality tests
-df_ssrf = pd.DataFrame()
-df_ssrchi = pd.DataFrame()
-df_lrtest = pd.DataFrame()
-df_parmf = pd.DataFrame()
+def conf_m_analysis(df, thresholds, m):
+    metrics = []
+    for threshold in thresholds:
+        df['prediction'] = df[df['prediction']] = 0
+
+        df.loc[df['score'] > threshold, ['prediction']] = 1
+
+        conf_m = pd.crosstab(df['flag'], df['prediction'])
+
+        T = (len(df))
+        P = len(df[df['flag'] == 1])
+        N = T - P
+        TN = conf_m[0][0]
+        FP = conf_m[0][1]
+        FN = conf_m[1][0]
+        TP = conf_m[1][1]
+
+        acc = accuracy(TP, TN, FP, FN)
+        sen = sensitivty(TP, P)
+        spe = specificity(TN, N)
+        kappa = cohens_kappa(TP, TN, FP, FN, T)
+
+        metrics.append({'test': m, 'threshold': threshold,
+                        'acc':  acc, 'sen': sen, 'spe': spe, 'kappa': kappa})
+    return metrics
 
 
-for group in groups:
-    gr = df[df['GroupKey'] == group]
-    pks = pd.unique(gr['PrimaryKey'])
-    root = df[df['PrimaryKey'] == group][['Date', 'Value']]
+def accuracy(TP, TN, FP, FN):
+    return (TP+TN)/(TP+TN+FP+FN)
 
-    _ssrf = {}
-    _ssrchi = {}
-    _lrtest = {}
-    _parmf = {}
 
-    num_lags = 3
-    pks = pks[1:]  # remove A1(root) form keys
-    for key in pks:
-        flag = df.loc[(df['PrimaryKey'] == key), 'DerivedFlag'].values[0]
-        rel = df[df['PrimaryKey'] == key][['Date', 'Value']]
-        VAR = pd.merge(root, rel, how='outer', on='Date')
+def sensitivty(TP, P):
+    return TP/P
 
-        gc = st.grangercausalitytests(
-            VAR[['Value_x', 'Value_y']], maxlag=num_lags, verbose=False)
 
-        ssrf = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
-        ssrchi = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
-        lrtest = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
-        parmf = {'score': 0, 'lag': 0, 'flag': flag, 'prediction': 0}
+def specificity(TN, N):
+    return TN/N
 
-        for lag, item in gc.items():
-            if item[0]['ssr_ftest'][0] > ssrf['score']:
-                ssrf['score'] = item[0]['ssr_ftest'][0]
-                ssrf['lag'] = lag
-            if item[0]['ssr_chi2test'][0] > ssrchi['score']:
-                ssrchi['score'] = item[0]['ssr_chi2test'][0]
-                ssrchi['lag'] = lag
-            if item[0]['lrtest'][0] > lrtest['score']:
-                lrtest['score'] = item[0]['lrtest'][0]
-                lrtest['lag'] = lag
-            if item[0]['params_ftest'][0] > parmf['score']:
-                parmf['score'] = item[0]['params_ftest'][0]
-                parmf['lag'] = lag
 
-        _ssrf[key] = ssrf
-        _ssrchi[key] = ssrchi
-        _lrtest[key] = lrtest
-        _parmf[key] = parmf
+# random accuracy
+def rand_acc(TP, TN, FP, FN, T):
+    return ((TN+FP)*(TN+FN)+(FN+TP)*(FP+TP))/(T*T)
 
-    df_ssrf = pd.concat(
-        [df_ssrf, pd.DataFrame.from_dict(_ssrf, orient='index')])
-    df_ssrchi = pd.concat(
-        [df_ssrchi, pd.DataFrame.from_dict(_ssrchi, orient='index')])
-    df_lrtest = pd.concat(
-        [df_lrtest, pd.DataFrame.from_dict(_lrtest, orient='index')])
-    df_parmf = pd.concat(
-        [df_parmf, pd.DataFrame.from_dict(_parmf, orient='index')])
 
-print(df_ssrf)
-
-threshold = 15
-
-df_ssrf.loc[df_ssrf['score'] > threshold, ['prediction']] = 1
-
-print(df_ssrf)
-
-confusion_matrix = pd.crosstab(df_ssrf['flag'], df_ssrf['prediction'])
-
-print(confusion_matrix)
+# https://www.standardwisdom.com/2011/12/29/confusion-matrix-another-single-value-metric-kappa-statistic/
+def cohens_kappa(TP, TN, FP, FN, T):
+    return 1-((1-accuracy(TP, TN, FP, FN)) / (1-rand_acc(TP, TN, FP, FN, T)))
 
 
 # SHOW HEAT MAP DA MATRIZ DE CONFUSÃO
-sn.heatmap(confusion_matrix, annot=True, fmt='.1f')
-plt.show()
+# conf_m = conf_m.apply(lambda r: r/r.sum(), axis=1)  # percentagens
+# sn.heatmap(conf_m, annot=True, fmt='.2f')
+# plt.show()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
